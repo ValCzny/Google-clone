fcc0477f6dfd5f07ba8015a37066a19f12fe45e6
Valentina Zapotoczny <valen.czny@gmail.com>
2023-02-23 12:12:56 -0300

    termine el codiigo js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// variables
 let input = document.getElementById("input");
+let phrase
+function cd() {
+    window.location.href = "https://www.google.com/search?q=" + phrase;
+}
+// evento del Enter
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        phrase = input.value;
+        cd()
+    }
+});
