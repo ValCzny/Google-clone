ed950d7e756833ebd96681138c19f8629364abbd
Valentina Zapotoczny <valen.czny@gmail.com>
2023-02-27 14:07:12 -0300

    agregué funcionalidad al js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // variables
 let input = document.getElementById("input");
+let boton_buscar = document.getElementById("boton-buscar");
 let phrase
+
+// función
 function cd() {
     window.location.href = "https://www.google.com/search?q=" + phrase;
 }
-// evento del Enter
+
+// evento de buscar
 document.addEventListener("keyup", function(event) {
     if (event.key === 'Enter') {
         phrase = input.value;
         cd()
     }
 });
+
+boton_buscar.addEventListener("click", function(event) {
+    phrase = input.value;
+    cd()
+})
